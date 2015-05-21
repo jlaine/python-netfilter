1e68035e223a6b6ed7134e2a6e5d1e32dd98046d
Jeremy Lainé <jeremy.laine@m4x.org>
2015-05-21 11:10:32 +0200

    move unit tests

--- setup.py
+++ setup.py
@@ -8,11 +8,13 @@ setup(
     description='Python modules for manipulating netfilter rules',
     url='https://github.com/jlaine/python-netfilter',
     author='Jeremy Laine',
+    author_email='jeremy.laine@m4x.org',
     license='GPLv3+',
     packages=['netfilter'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-    ]
+    ],
+    test_suite='tests',
 )
