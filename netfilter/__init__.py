2002aef3792c30fd69688f6767b9334e6c532839
Jeremy Lainé <jeremy.laine@m4x.org>
2015-05-21 10:37:25 +0200

    bump version to 0.5.10

--- netfilter/__init__.py
+++ netfilter/__init__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 """
-python-netfilter
-Bolloré Telecom
+python-netfilter - Python modules for manipulating netfilter rules
 
-@author: Jeremy Lainé <jeremy.laine@bolloretelecom.eu>
+@author: Jeremy Lainé <jeremy.laine@m4x.org>
 """
 __docformat__ = 'epytext en'
 
-__version__ = '0.5.9'
-__url__ = 'http://opensource.bolloretelecom.eu/projects/python-netfilter'
-__copyright__ = '(C) 2007-2012 Bolloré Telecom'
+__version__ = '0.5.10'
+__url__ = 'https://github.com/jlaine/python-netfilter'
+__copyright__ = '(C) 2007-2012 Bolloré Telecom, 2013 Jeremy Lainé'
 __license__ = 'GPLv3'
 __license_long__ = """
  Copyright (C) 2007-2012 Bolloré Telecom
+ Copyright (C) 2013 Jeremy Lainé
  See AUTHORS file for a full list of contributors."""
 
