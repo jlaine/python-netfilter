093763b888d25d5afc41277577ed721b94d57d35
Jeremy Lainé <jeremy.laine@m4x.org>
2015-05-21 10:58:23 +0200

    remove AUTHORS file, update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ from setuptools import setup
 
 setup(
     name='python-netfilter',
-    version='0.5.10',
+    version='0.6.0',
     description='Python modules for manipulating netfilter rules',
     url='https://github.com/jlaine/python-netfilter',
     author='Jeremy Laine',
-    license='GPLv3',
-    packages=['netfilter'])
+    license='GPLv3+',
+    packages=['netfilter'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    ]
+)
