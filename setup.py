a9cec6b76e942ab19cf3ad8d5ba7cc4cc911a54b
Jeremy Lainé <jeremy.laine@m4x.org>
2015-05-28 08:32:51 +0200

    add some categorizers

--- setup.py
+++ setup.py
@@ -14,7 +14,15 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Networking :: Firewalls',
+        'Topic :: System :: Systems Administration',
     ],
     test_suite='tests',
 )
